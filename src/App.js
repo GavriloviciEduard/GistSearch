f407b9059ff8adefd7df9fcba2dcb2a2293e762b
Eddie Gavrilovici <eddiegavrilovici@gmail.com>
2021-06-29 08:41:42 +0300

    Added search component.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import SearchBar from "./components/SearchBar/SearchBar";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,36 @@
+import React, { useRef } from "react";
+import classes from "./SearchBar.css";
+import { useGlobalContext } from "../../context";
+import axios from "axios";
+
+const SearchBar = () => {
+  const { updateGists, updateUsername } = useGlobalContext();
+  const user = useRef("");
+
+  const handleKeyDown = (event) => {
+    if (user.current.value === "" || event.key !== "Enter") {
+      return;
+    }
+    updateUsername(user.current.value);
+    axios
+      .get(`https://api.github.com/users/${user.current.value}/gists`)
+      .then((response) => {
+        updateGists(response.data);
+      });
+  };
+
+  return (
+    <div className={classes.SearchBar}>
+      <div className={classes.SearchContent}>
+        <p>GistSearch</p>
+        <input
+          onKeyDown={(event) => handleKeyDown(event)}
+          ref={user}
+          placeholder="Search..."
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
